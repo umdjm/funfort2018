0e868dcd3752838d2b690561eb079e77cf8ee891
DJ <umdjm@hotmail.com>
2018-10-05 21:12:34 -0400

    ui router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,26 @@
 'use strict';
 
-angular
+var app = angular
   .module('funfortApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ui.router',
+    'angular-md5',
+    'firebase'
   ]);
+
+  app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'auth/login.html'
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'auth/register.html'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  });
